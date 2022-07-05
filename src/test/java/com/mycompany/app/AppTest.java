873eb46a922d7cffcb452c566ffffcc269ec1647
Sergio <sergio.gutierrez4@ibm.com>
2022-07-05 17:10:15 +0200

    remove test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,48 +1,48 @@
-package com.mycompany.app;
+// package com.mycompany.app;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.After;
-import static org.junit.Assert.*;
+// import java.io.ByteArrayOutputStream;
+// import java.io.PrintStream;
+// import org.junit.Before;
+// import org.junit.Test;
+// import org.junit.After;
+// import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-{
+// /**
+//  * Unit test for simple App.
+//  */
+// public class AppTest
+// {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
+//     @Before
+//     public void setUpStreams() {
+//         System.setOut(new PrintStream(outContent));
+//     }
 
-    @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-        } catch (Exception e) {
-            fail("Construction failed.");
-        }
-    }
+//     @Test
+//     public void testAppConstructor() {
+//         try {
+//             new App();
+//         } catch (Exception e) {
+//             fail("Construction failed.");
+//         }
+//     }
 
-    @Test
-    public void testAppMain()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World, Sergio!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World, Sergio!\"");
-        }
-    }
+//     @Test
+//     public void testAppMain()
+//     {
+//         App.main(null);
+//         try {
+//             assertEquals("Hello World, Sergio!" + System.getProperty("line.separator"), outContent.toString());
+//         } catch (AssertionError e) {
+//             fail("\"message\" is not \"Hello World, Sergio!\"");
+//         }
+//     }
 
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
+//     @After
+//     public void cleanUpStreams() {
+//         System.setOut(null);
+//     }
 
-}
+// }
